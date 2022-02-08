7d3a400b0e6c6350b47b19293b5c4f16f45d5c16
Hugo <hmoralesg@icloud.com>
2022-02-08 03:31:56 -0300

    update: dark theme enabled by default

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script lang="ts" setup>
 // Use Auth
 import useAuth from '@composables/auth';
+import { darkTheme } from 'naive-ui';
 
 useAuth();
 </script>
 
 <script lang="ts">
+
 export default {
   name: 'app-base',
 };
 </script>
 
 <template lang="pug">
-n-config-provider
+n-config-provider(:theme="darkTheme")
   router-view
 </template>

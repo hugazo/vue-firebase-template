c33b62a01063e38e043a1fb069560f4263218abe
Hugo <hmoralesg@icloud.com>
2022-02-20 14:27:31 -0300

    fix: updated auth guards to be applied at router level

---
+++ src/guards.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+
+import { RouteLocation } from 'vue-router';
+
+import authStore from './store/auth';
+
+
+const authGuard = (
+  to: RouteLocation,
+) => {
+  const auth = authStore();
+  if (to.meta.requiresAuth && !auth.logged) {
+    return { name: 'sign-in' };
+  }
+  return true;
+};
+
+export default authGuard;
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 // Base import
 import { createApp } from 'vue';
 
-// Router Imports
-import { createRouter, createWebHistory } from 'vue-router';
-import { setupLayouts } from 'virtual:generated-layouts';
-import generatedRoutes from 'virtual:generated-pages';
+// Head
+import { createHead } from '@vueuse/head';
 
 // State Management
 import { createPinia } from 'pinia';
 import piniaPersist from 'pinia-plugin-persist';
 
-// Head
-import { createHead } from '@vueuse/head';
+// Router Imports
+import { createRouter, createWebHistory } from 'vue-router';
+import { setupLayouts } from 'virtual:generated-layouts';
+import generatedRoutes from 'virtual:generated-pages';
+import authGuard from './guards';
 
 // Base Component
 import App from './App.vue';
@@ -19,7 +20,6 @@ import App from './App.vue';
 // App setup
 const app = createApp(App);
 
-// Route & layouts setup
 const routes = setupLayouts(generatedRoutes);
 
 const router = createRouter({
@@ -27,13 +27,17 @@ const router = createRouter({
   routes,
 });
 
-// Add the router to app
-app.use(router);
-
 // Add the state management to app
 const pinia = createPinia();
 pinia.use(piniaPersist);
 app.use(pinia);
+
+// Route & layouts setup
+
+router.beforeEach(authGuard);
+
+// Add the router to app
+app.use(router);
 
 const head = createHead();
 app.use(head);

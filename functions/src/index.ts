113adfcb43d494b457182424cbe692cb805bf604
Hugo <hmoralesg@icloud.com>
2022-03-07 01:00:41 -0300

    disabled unused firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,10 @@
+// import * as functions from 'firebase-functions';
 
-import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
-// eslint-disable-next-line import/prefer-default-export
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+/*
+* The following is only an example of firebase functions.
+* For more information please visit: https://firebase.google.com/docs/functions
+ */
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info('Hello logs!', { structuredData: true });
+//   response.send('Hello from Firebase!');
+// });

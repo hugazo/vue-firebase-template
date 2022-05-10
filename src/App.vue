dece85bc815841577c9e7ab676bd8d5990309579
Hugo <hmoralesg@icloud.com>
2022-05-09 22:33:04 -0400

    refactored app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script lang="ts" setup>
 // Use Auth
 import useAuth from '@composables/auth';
-import themeStore from '@store/theme';
-
-const theme = themeStore();
-
-const currentTheme = computed(() => theme.getTheme.resolver());
 
 useAuth();
 </script>
@@ -18,6 +13,5 @@ export default {
 </script>
 
 <template lang="pug">
-n-config-provider(:theme="currentTheme")
-  router-view
+router-view
 </template>
